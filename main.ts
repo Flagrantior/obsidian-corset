b136a8c33f2d036c6f3ab3ba09c179245007bce6
Flagrantior <flagrantior@gmail.com>
2023-07-25 19:59:27 +0300

    Update main.ts
    Renaming

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 import { Plugin } from 'obsidian';
 
-export default class Shirr extends Plugin {
+export default class Corset extends Plugin {
 	async onload() {
 		const root = document.querySelector<HTMLElement>(':root')?.style;
-		root?.setProperty('--flagrantior-shirr-width', '100%');
+		root?.setProperty('--flagrantior-corset-width', '100%');
 		const sheet = document.styleSheets[0];
 		sheet?.insertRule('.markdown-preview-scroller, .cm-scroller {justify-content: center}', sheet.cssRules.length)
-		sheet?.insertRule('.markdown-preview-sizer, .cm-sizer {width: var(--flagrantior-shirr-width) !important}', sheet.cssRules.length)
+		sheet?.insertRule('.markdown-preview-sizer, .cm-sizer {width: var(--flagrantior-corset-width) !important}', sheet.cssRules.length)
 		this.registerDomEvent(document, 'wheel', (evt: MouseEvent) => {	if (evt.altKey) {
-			root?.setProperty('--flagrantior-shirr-width',
+			root?.setProperty('--flagrantior-corset-width',
 				Math.max(Math.min(
-					(+(root.getPropertyValue('--flagrantior-shirr-width').slice(0, -1))-(<any>evt).deltaY*0.01),
+					(+(root.getPropertyValue('--flagrantior-corset-width').slice(0, -1))-(<any>evt).deltaY*0.01),
 					100), 20).toString()+'%');
 		}});
 	}
